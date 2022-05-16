35310bc32c3ec112cd240df0c27d6286c70b8911
Albertabrahams <halilibrahim.soyman@gmail.com>
2022-05-16 20:14:09 +0300

    started

--- Models/doritos/admin.py
+++ Models/doritos/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Customer, Type
+from .models import Customer, Customer2, Type
 
 admin.site.register(Type)
 admin.site.register(Customer)
+admin.site.register(Customer2)
--- Models/doritos/models.py
+++ Models/doritos/models.py
@@ -9,3 +9,14 @@ class Customer(models.Model):
     name = models.CharField(max_length=20)
     surname = models.CharField(max_length=20)
     mature = models.BooleanField()
+
+    def __str__(self):
+        return "{} {} {}".format(self.name, self.surname, self.mature)
+
+class Customer2(models.Model):
+    name = models.CharField(max_length=20)
+    surname = models.CharField(max_length=20)
+    mature = models.BooleanField()
+
+    def __str__(self):
+        return "{} {} {}".format(self.name, self.surname, self.mature)
