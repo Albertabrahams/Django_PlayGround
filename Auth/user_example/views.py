90a2557031e64f292fe169d298411fb382647ef5
Albertabrahams <halilibrahim.soyman@gmail.com>
2022-05-27 18:40:48 +0300

    1.2

--- Auth/user_example/views.py
+++ Auth/user_example/views.py
@@ -1,16 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from sympy import re
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+
 
 def home_view(request):    
     return render(request, "user_example/home.html")
 
-
+@login_required
 def special(request):    
     return render(request, "user_example/special.html")
 
 def register(request):
-    form = UserCreationForm()
+    form = UserCreationForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+
+        user = authenticate(username=username, 
+        password=password)
+        login(request, user)
+        return redirect("home")
     context = {
         'form':form
     }
