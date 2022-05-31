27d6f71e63eb831740f31648f0581870abd80455
Albertabrahams <halilibrahim.soyman@gmail.com>
2022-05-31 22:46:10 +0300

    model

--- Auth_Base_User/users/models.py
+++ Auth_Base_User/users/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, User
 
 class User(AbstractUser):
     portfolio = models.URLField(blank=True)
