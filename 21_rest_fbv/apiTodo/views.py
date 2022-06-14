2d6541b6bfcb2e4f209bc7c0764a269c446c3539
Albertabrahams <halilibrahim.soyman@gmail.com>
2022-06-14 14:24:56 +0300

    1.2

--- 21_rest_fbv/apiTodo/views.py
+++ 21_rest_fbv/apiTodo/views.py
@@ -45,21 +45,23 @@ def todoListCreate(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET','PUT'])
+@api_view(['GET','PUT','DELETE'])
 def todoUpdate(request, pk):
     queryset=Todo.objects.get(id=pk)
     if request.method == 'GET':
         serializer = TodoSerializer(queryset)
         return Response(serializer.data)
+        
     elif request.method == 'PUT':
         serializer = TodoSerializer(instance=queryset, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     elif request.method == 'DELETE':
         queryset.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response("Item deleted successfully", status=status.HTTP_204_NO_CONTENT)
     
 @api_view(['DELETE'])
 def todoDelete(request, pk):
